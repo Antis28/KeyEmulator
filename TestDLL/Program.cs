57e8fbae6bde6f26f574804c108da278bf95581f
Antis <antis28@mail.ru>
2023-07-09 17:54:25 +0300

    add тестирование библиотеки в консоли

---
+++ TestDLL/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KeyboardEmulator;
+using KeyEmulator.WindowWorkers;
+
+namespace TestDLL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {           
+            IntPtr hWnd = WorkerWithWindows.GetDesktopWindow();
+            RECT rect = new RECT();
+            WorkerWithWindows.GetWindowRect(hWnd, out rect);
+            int width = rect.right - rect.left;
+            int height = rect.bottom - rect.top;
+
+
+            WinAPI.MouseMove(rect.right -170, rect.bottom -125);
+        }
+    }
+}
